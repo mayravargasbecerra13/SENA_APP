ea662dda2d986e24d0be76acc8e0552617a0dc06
Mayra Vargas <vargasmayra800@gmail.com>
2025-07-30 19:28:13 -0500

    Se añadió Templates para interfaz de lista de aprendices

--- aprendices/models.py
+++ aprendices/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 
+
 # Create your models here.
-class Aprendiz(models.model):
+class Aprendiz(models.Model):
     documento_identidad = models.CharField(max_length=100, unique=True)
     nombre = models.CharField(max_length=185, unique=True)
     apellido = models.CharField(max_length=185, unique=True)
@@ -10,6 +11,7 @@ class Aprendiz(models.model):
     fecha_nacimiento = models.DateTimeField(null=True)
     ciudad = models.CharField(max_length=185, null=True)
     programa = models.CharField(max_length=185)
-    
+
+
 def __str__(self):
     return f"{self.nombre} {self.apellido} - {self.documento_identidad}"
